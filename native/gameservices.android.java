382caafd36914c373478b6216b606e47b2f0bba5
Dominik Kollon <d.kollon@blackbird-design.de>
2014-05-28 19:08:10 +0200

    Added isNetworkAvailable / GPGS now don't autologin during onResume
    Added function isNetworkAvailable to check if an internet connection is available.
    Fixed autologin of Google Play Game Services during OnResume.

--- native/gameservices.android.java
+++ native/gameservices.android.java
@@ -1,4 +1,3 @@
-
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -6,6 +5,7 @@ import android.os.Bundle;
 import android.os.MessageQueue.IdleHandler;
 import android.provider.MediaStore;
 import android.app.Fragment;
+import android.net.ConnectivityManager;
 
 import com.google.android.gms.games.achievement.*
 
@@ -14,27 +14,27 @@ class BBGameService extends ActivityDelegate {
 	GameHelper mHelper;
 	BBGameService parent;
 	GameHelper.GameHelperListener listener;
-	
+
 	boolean _running;
-	
+
 	int _result=-1;
 	int REQUEST_LEADERBOARD = 9101;
 	int REQUEST_ACHIEVEMENTS = 9102;
-	
+
 	class GameServiceThread extends Thread{
-		
+
 		GameServiceThread(){
 			_running=true;	
 		}
-		
+
 		public void run(){
 
 			Looper.prepare();
-			
+
 			MessageQueue queue = Looper.myQueue();
 			queue.addIdleHandler(new IdleHandler() {
 				int mReqCount = 0;
-				
+
 				@Override
 				public boolean queueIdle() {
 				if (++mReqCount == 2) {
@@ -47,11 +47,12 @@ class BBGameService extends ActivityDelegate {
 
 			mHelper = new GameHelper(_activity, GameHelper.CLIENT_ALL);
     		mHelper.setup(parent.listener);
-			
+			mHelper.setMaxAutoSignInAttempts(0);
+
 			Looper.loop();
 		}
 	}
-	
+
 	@Override
     public void onStart() {
         super.onStart();
@@ -63,18 +64,29 @@ class BBGameService extends ActivityDelegate {
         super.onStop();
 		mHelper.onStop();
     }
-	
+
 	@Override	
 	public void onActivityResult( int requestCode,int resultCode,Intent data ){
 		super.onActivityResult(requestCode, resultCode, data);
 		mHelper.onActivityResult(requestCode, resultCode, data);
 	}
 	
+	public boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) _activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isAvailable()
+                && cm.getActiveNetworkInfo().isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+
 	public BBGameService(){
 
 		_activity=BBAndroidGame.AndroidGame().GetActivity();
 		parent = this;
-		
+
 		listener = new GameHelper.GameHelperListener() {
 	       @Override
 		    public void onSignInSucceeded() {
@@ -85,53 +97,55 @@ class BBGameService extends ActivityDelegate {
 				// handle sign-in failure - reserved for later use
 			}
 	    };
-		
+
 		GameServiceThread thread=new GameServiceThread();
 		_running=true;
 		thread.start();
-		
+
 		BBAndroidGame.AndroidGame().AddActivityDelegate( this );
-		
+
 	}
-	
+
 	public void submitHighscore(String id, int points) {
 		Games.Leaderboards.submitScore(mHelper.getApiClient(), id, points);
 	}
-	
+
 	public void unlockAchievement(String id) {
 		Games.Achievements.unlock(mHelper.getApiClient(), id);
 	}
-	
+
 	public void revealAchievement(String id) {
 		Games.Achievements.reveal(mHelper.getApiClient(), id);
 	}
-	
+
 	public void incrementAchievement(String id, int step) {
 		Games.Achievements.increment(mHelper.getApiClient(), id, step);
 	}
-	
+
 	public void showLeaderBoard(String id) {
 		_activity.startActivityForResult(Games.Leaderboards.getLeaderboardIntent(mHelper.getApiClient(), id), REQUEST_LEADERBOARD);
 	}
-	
+
 	public void showAchievements() {
 		_activity.startActivityForResult(Games.Achievements.getAchievementsIntent(mHelper.getApiClient()), REQUEST_ACHIEVEMENTS);
 	}
-	
+
 	public boolean isLoggedIn() {
 		return mHelper.isSignedIn();
 	}
-	
+
 	public void signOut() {
 		mHelper.signOut();
 	}
-	
+
 	public void beginUserSignIn() {
-		mHelper.beginUserInitiatedSignIn();
+		if(isNetworkAvailable()) {
+			mHelper.beginUserInitiatedSignIn();
+		}
 	}
-	
+
 	public void setMaxUserSignIns(int count) {
 		mHelper.setMaxAutoSignInAttempts(count);
 	}
-	
+
 }
